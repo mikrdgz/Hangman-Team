49b9322ec286cd4c3399597c601c0646619dc800
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-05-11 18:57:01 -0500

    cant add user guess to DOM

--- hangman.js
+++ hangman.js
@@ -4,32 +4,42 @@ const characters = ["Thor", "Spiderman", "Starlord", "Hulk", "Rocket"];
 
 //generate hangman word
 
-function getWord(arr) {
-  let word = arr[Math.floor(Math.random() * arr.length)];
-  console.log(word);
+let word = characters[Math.floor(Math.random() * characters.length)];
 
+//Start with - for unanswered letters. Must be the length of the word.
+let answerArray = [];
 
-  let answerArray = [];
- for (let i = 0; i < word.length; i++) {
-   answerArray[i] = "_";
- }
- for (let i = 0; i <= word.length; i++) {
+function getWordArr() {
+  for (let i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+  }
+  for (let i = 0; i <= word.length; i++) {
     let w = document.getElementById("word");
     w.innerText = answerArray;
   }
 }
 
 // We need an input to guess a letter
+let userGuess = [];
 
-function getPrompt() {}
+function getPrompt() {
+  userGuess = document.getElementById("guess").value;
+}
 
 function hangMan() {
-  getWord(characters);
+  getWordArr(characters);
+}
+
+function checkValue() {
+  getPrompt();
+  for (let i = 0; i <= word.length; i++) {
+    if (word[i] === userGuess) {
+      //I cant figure out how to insert the users guess in the answerArray!
+    }
+  }
 }
 
 hangMan();
-
-//Start with - for unanswered letters. Must be the length of the word.
 
 //If that letter is not a match, we need to print 'Not a match'
 
