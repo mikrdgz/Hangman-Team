c7dba8568ff3abffc830cb392fce00e4c31caf44
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-05-11 19:14:52 -0500

    adding mikaela to master

--- hangman.js
+++ hangman.js
@@ -1,48 +1,45 @@
-'use strict';
+"use strict";
 
+const characters = ["Thor", "Spiderman", "Starlord", "Hulk", "Rocket"];
 
+//generate hangman word
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function getPrompt() {
-    hangMan();
-    rl.question('Guess: ', () => {
-    });
-  }
-  getPrompt();
-
-  const characters = [
-    "Thor",
-    "Spiderman",
-    "Starlord",
-    "Hulk",
-    "Rocket"
-]
-
-function makeDashes(){
-var word = words[Math.floor(Math.random() * words.length)];
-
-// Set up the amswer answerArrayvar answerArray = [];
-for (var i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
-}
-}
-
-
-function hangMan() {
-
-}
+let word = characters[Math.floor(Math.random() * characters.length)];
 
 //Start with - for unanswered letters. Must be the length of the word.
+let answerArray = [];
+
+function getWordArr() {
+  for (let i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+  }
+  for (let i = 0; i <= word.length; i++) {
+    let w = document.getElementById("word");
+    w.innerText = answerArray;
+  }
+}
 
 // We need an input to guess a letter
+let userGuess = [];
 
-//If that letter is a match in the word, then we replace dashes with that letter
+function getPrompt() {
+  userGuess = document.getElementById("guess").value;
+}
+
+function hangMan() {
+  getWordArr(characters);
+}
+
+function checkValue() {
+  getPrompt();
+  for (let i = 0; i <= word.length; i++) {
+    if (word[i] === userGuess) {
+      //I cant figure out how to insert the users guess in the answerArray!
+    }
+  }
+}
+
+hangMan();
 
 //If that letter is not a match, we need to print 'Not a match'
 
